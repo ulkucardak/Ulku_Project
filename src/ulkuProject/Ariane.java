84e7772f80635c60a3276855c882db63a8a21bd7
Hakan Gok <hakangok1974@yahoo.com>
2022-04-27 19:29:24 +0100

    3rd commit

--- src/ulkuProject/Ariane.java
+++ src/ulkuProject/Ariane.java
@@ -3,4 +3,7 @@ package ulkuProject;
 public class Ariane {
     // 1st commit
     // second commit
+    public static void main(String[] args) {
+        System.out.println("and a little message");
+    }
 }
