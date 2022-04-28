ea5afc2e9c73070045053a33826d1cf7a69b89cb
mervecakir <m.cakir88w@gmail.com>
2022-04-28 12:25:53 +0100

    first conflict

--- src/ulkuProject/Ariane.java
+++ src/ulkuProject/Ariane.java
@@ -1,10 +1,26 @@
 package ulkuProject;
-
 public class Ariane {
     // 1st commit
     // second commit
     public static void main(String[] args) {
         System.out.println("and a little message");
+
+
+        int x=9;
+        System.out.println(x+10);
+
+
+
     }
+
+
+
+
 }
+
+
+
+
+
+
 
